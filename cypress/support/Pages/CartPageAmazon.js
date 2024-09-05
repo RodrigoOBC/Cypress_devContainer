900e14bba8ce9212ff5cbce43f6a74ba3454b0e5
rodrigo.cabral <rodrigo.cabral@radixeng.com>
2024-09-05 17:52:57 -0300

    colocando o todo

--- cypress/support/Pages/CartPageAmazon.js
+++ cypress/support/Pages/CartPageAmazon.js
@@ -1,3 +1,4 @@
+// TODO criar page object para a página de carrinho do Amazon
 class CartPageAmazon {
   // Add class content here
 }
--- cypress/support/Pages/ProductPageAmazon.js
+++ cypress/support/Pages/ProductPageAmazon.js
@@ -1,3 +1,5 @@
+
+// TODO criar page object para a página de produto do Amazon
 class ProductPageAmazon {
     // Métodos e propriedades da classe aqui
 }
